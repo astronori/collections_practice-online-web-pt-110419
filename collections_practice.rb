3273cfd9e5d6279927dac1ce8dccbf7fdd8b5ceb
kenneth gil <astronori@gmail.com>
2019-12-03 23:36:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,6 @@ def sort_array_asc(array)
 array.sort
 end
 
-sort_array_asc = ["bday" "happy" "sensual" "ruby" "apple"]
+sort_array_asc (["bday", "happy", "sensual", "ruby", "apple"]
 
 sort_array_asc.sort
